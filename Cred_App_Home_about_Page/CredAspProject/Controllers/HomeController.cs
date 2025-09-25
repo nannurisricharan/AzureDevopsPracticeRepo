5a7c31e01d06cdba9be09c206239f3b707e86d4e
CharanNannuri <nannurisricharan@gmail.com>
2025-09-24 21:34:00 -0500

    commit all

---
+++ Cred_App_Home_about_Page/CredAspProject/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+using System.Diagnostics;
+using CredAspProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CredAspProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // About page
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
